dfddd6d585824abed9beb309c6bfede1c57f5640
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-03-07 17:10:56 +0900

    remove: remove commander

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@
 
 import findUnpairedTags from './findUnpairedTags';
 import format from './format';
-import { program } from 'commander';
 import fs from 'node:fs';
 
-program
-  .command('file <filePath>')
-  .description('Find unpaired tags in a file')
-  .action((filePath) => {
-    const data = fs.readFileSync(filePath, 'utf8');
-    const unpairedTags = findUnpairedTags(data);
-    console.log(format(unpairedTags, filePath));
-  });
-
-program.parse();
+const filePaths = process.argv.slice(2);
+filePaths.forEach((filePath) => {
+  const data = fs.readFileSync(filePath, 'utf8');
+  const unpairedTags = findUnpairedTags(data);
+  const formatted = format(unpairedTags, filePath);
+  console.log(`target file: ${filePath}`);
+  if (formatted.length === 0) {
+    console.log('No unpaired tags found');
+  } else {
+    console.log(formatted);
+  }
+});

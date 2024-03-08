d2ec897077b772decc44867e989e039ed9dfe42b
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-03-08 12:23:46 +0900

    add: ファイルの種類に応じて処理を変更するようにした

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,23 @@
 import findUnpairedTags from './findUnpairedTags';
 import { Tag } from './tag';
 import fs from 'node:fs';
-import { escapeComment, escaper } from './escape';
+import path from 'node:path';
+import { escapeRegexps, escaper } from './escape';
+
+const fileTypeToEscapes = (fileType: string) => {
+  if (fileType.match(/md/i) || fileType.match(/markdown/i)) {
+    return Object.values(escapeRegexps);
+  } else {
+    return [escapeRegexps.comment];
+  }
+};
 
 const filePaths = process.argv.slice(2);
 filePaths.forEach((filePath) => {
   const data = fs.readFileSync(filePath, 'utf8');
-  const escaped = escaper(data, [escapeComment]);
+  const fileType = path.extname(filePath).slice(1);
+  const escapes = fileTypeToEscapes(fileType);
+  const escaped = escaper(data, escapes);
   const unpairedTags = findUnpairedTags(escaped);
   const formatted = Tag.formatMessage(unpairedTags, filePath);
   console.log(`target file: ${filePath}`);
